4d4b3456b83b77ee0e1793d387bb4f886fa2ec21
txilla <txilla@users.noreply.github.com>
2017-10-19 17:30:05 +0200

    add logo and save data to SharedPreferences

--- app/src/main/java/com/example/txilla/domometeo/ConfigurationActivity.java
+++ app/src/main/java/com/example/txilla/domometeo/ConfigurationActivity.java
@@ -1,16 +1,21 @@
 package com.example.txilla.domometeo;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Patterns;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class ConfigurationActivity extends AppCompatActivity {
 
@@ -19,14 +24,33 @@ public class ConfigurationActivity extends AppCompatActivity {
     @BindView(R.id.usernameEditText) EditText username;
     @BindView(R.id.passwordEditText) EditText password;
     @BindView(R.id.pbConfig) ProgressBar pb;
+    @BindView(R.id.saveButton) Button buttonSave;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_configuration);
         ButterKnife.bind(this);
+
+        SharedPreferences sharedPref = this.getPreferences(Context.MODE_PRIVATE);
+        String serverSaved = sharedPref.getString("serverAdress","noAdress");
+        String portSaved = sharedPref.getString("port","noPort");
+        String usernameSaved = sharedPref.getString("username","noUsername");
+        String passwordSaved = sharedPref.getString("password","noPassword");
+
+        if ( !serverSaved.equals("noAdress") && !portSaved.equals("noPort")
+                && !usernameSaved.equals("noUsername") && !passwordSaved.equals("noPassword")) {
+            startActivity(new Intent(this, MainActivity.class));
+        }
     }
 
-    private void saveData() {
+    @OnClick(R.id.saveButton)
+    public void save() {
+        pb.setVisibility(View.VISIBLE);
+        buttonSave.setEnabled(false);
+        clickSave();
+    }
+
+    private void clickSave() {
 
         String textToast = null;
 
@@ -35,10 +59,11 @@ public class ConfigurationActivity extends AppCompatActivity {
         String usernameText = username.getText().toString().trim();
         String passwordText = password.getText().toString().trim();
 
-        if (TextUtils.isEmpty(serverAdressText) || TextUtils.isEmpty(portText) || TextUtils.isEmpty(usernameText) || TextUtils.isEmpty(passwordText)) {
+        if (TextUtils.isEmpty(serverAdressText) || TextUtils.isEmpty(portText)
+                || TextUtils.isEmpty(usernameText) || TextUtils.isEmpty(passwordText)) {
             textToast = getString(R.string.emptyFields);
         } else if (!Patterns.IP_ADDRESS.matcher(serverAdressText).matches()) {
-            //textToast = getString(R.string.emailNoExist);
+            textToast = getString(R.string.validIP);
         }
         /*else if (password.length() < 8) {
             //textToast = getString(R.string.passwordLength);
@@ -51,8 +76,25 @@ public class ConfigurationActivity extends AppCompatActivity {
             //menuItemSave.setEnabled(true);
         } else {
 
-            //register(username, password, mail);
+            saveData();
 
         }
+    }
+
+    private void saveData() {
+
+        SharedPreferences sharedPref = this.getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+        //editor.putInt(getString(R.string.saved_high_score), newHighScore);
+        editor.putString("serverAdress",serverAdress.getText().toString());
+        editor.putString("port",port.getText().toString());
+        editor.putString("username",username.getText().toString());
+        editor.putString("password",password.getText().toString());
+        editor.commit();
+
+        pb.setVisibility(View.INVISIBLE);
+
+        startActivity(new Intent(this, MainActivity.class));
+
     }
 }
