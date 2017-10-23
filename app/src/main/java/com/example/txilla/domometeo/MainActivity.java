f63720704e6f06b2c4dbe09003338a778a2083ff
txilla <txilla@users.noreply.github.com>
2017-10-23 17:32:08 +0200

    create DashboardFragment

--- app/src/main/java/com/example/txilla/domometeo/MainActivity.java
+++ app/src/main/java/com/example/txilla/domometeo/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.txilla.domometeo;
 
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.txilla.domometeo.Fragments.DashboardFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                .replace(R.id.containerView, DashboardFragment.newInstance())
+                .commit();
     }
 }
